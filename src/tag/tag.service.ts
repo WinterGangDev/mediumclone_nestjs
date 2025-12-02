ec7a22eeead0472a72e746c3b1e569ce56938e06
mehrad <y.nakhli1383@gmail.com>
2025-12-03 01:07:54 +0330

    fix(TagService): correct Repository type from TagEntity[] to TagEntity

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from "@nestjs/common";
-
+import { Injectable } from '@nestjs/common';
+import { promises } from 'fs';
+import { Repository } from 'typeorm';
+import { TagEntity } from './tag.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 @Injectable()
 export class TagService {
-    findAll() :string[]{
-           return ['coffee','whater','hi'];
-    }
+  constructor(
+    @InjectRepository(TagEntity)
+    private readonly tagRepository: Repository<TagEntity>,
+  ) {}
+  async findAll(): Promise<TagEntity[]> {
+    return await this.tagRepository.find();
+  }
 }
